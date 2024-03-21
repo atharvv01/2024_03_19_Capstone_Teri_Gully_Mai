4126a10d2466bccddc87afd5b03631dc6cb76a5b
atharvv01 <athzanwa@gmail.com>
2024-03-21 17:46:03 +0530

    updated the excel to add two new apis

--- 05_Backend/controllers/blog_controller.js
+++ 05_Backend/controllers/blog_controller.js
@@ -1,4 +1,4 @@
-const Blog = require('../schema/blog_schema'); 
+const Blog = require('../schema/blog_schema');
 const Place = require('../schema/place_schema');
 const { verifyToken } = require("../middleware/auth");
 
@@ -14,7 +14,7 @@ const createBlog = async (req, res) => {
       description,
       city,
       thumbnail,
-      author:userData.userId
+      author: userData.userId
     });
     const savedBlog = await newBlog.save();
     res.status(201).json(savedBlog);
@@ -47,48 +47,51 @@ const addPlaceToBlog = async (req, res) => {
 };
 
 // controller to delete a place while creating a blog
-const placeToDelete =  async (req, res) => {
-    try {
-        const placeId = req.query.placeId;
-        
-        // Delete the place by ID
-        console.log(placeId + " is deleted");
-        await Place.findByIdAndDelete(placeId);
-        
-        // Update blog documents to remove the deleted place ID from the `places` array
-        await Blog.updateMany({ $pull: { places: placeId } });
-    
-        res.status(200).json({ message: 'Place deleted successfully' });
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
-  };
-  
+const placeToDelete = async (req, res) => {
+  try {
+    const placeId = req.query.placeId;
+
+    // Delete the place by ID
+    console.log(placeId + " is deleted");
+    await Place.findByIdAndDelete(placeId);
+
+    // Update blog documents to remove the deleted place ID from the `places` array
+    await Blog.updateMany({ $pull: { places: placeId } });
+
+    res.status(200).json({ message: 'Place deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 // Controller to fetch blog details by city ..here it also fetchs places inside to the blog
 const getBlogByCity = async (req, res) => {
-    try {
-        const city = req.query.city;
-        // Find blogs with the specified city
-        const blogs = await Blog.find({ city });
-        res.status(200).json(blogs);
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
-  };
+  try {
+    const city = req.query.city;
+    // Find blogs with the specified city
+    const blogs = await Blog.find({ city });
+    if (!blogs) {
+      return res.status(404).json({ success: false, message: "Blog not found" });
+    }
+    res.status(200).json(blogs);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 // Controller to fetch blog details by ID
 const getBlogById = async (req, res) => {
   try {
-      const blogId = req.query.blogId;
-      // Find the blog with the specified ID
-      const blog = await Blog.findById(blogId);
-      if (!blog) {
-          return res.status(404).json({ success: false, message: "Blog not found" });
-      }
-      res.status(200).json(blog);
+    const blogId = req.query.blogId;
+    // Find the blog with the specified ID
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res.status(404).json({ success: false, message: "Blog not found" });
+    }
+    res.status(200).json(blog);
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, message: "Internal Server Error" });
+    console.error(error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 
