bfeca90ec37c2650ebf8220c98c18ea063f58e01
AyanS1912 <92726765+AyanS1912@users.noreply.github.com>
2024-03-28 23:35:45 +0530

    Feature Branch and service created

--- 06_Frontend/src/app/components/smaller-blog/smaller-blog.component.ts
+++ 06_Frontend/src/app/components/smaller-blog/smaller-blog.component.ts
@@ -1,4 +1,5 @@
 import { Component,Input,Output,EventEmitter } from '@angular/core';
+import { SaveCollectionService } from '../../services/save-collection.service';
 
 @Component({
   selector: 'app-smaller-blog',
@@ -12,8 +13,23 @@ export class SmallerBlogComponent {
 
   @Output() blogClicked: EventEmitter<string> = new EventEmitter<string>(); // Event emitter for click event
 
+
+  constructor(private saveService: SaveCollectionService) { }
   onClick(): void {
     this.blogClicked.emit(this.blogId); // Emit the blog ID when clicked
+  }
+
+  onSave(): void {
+    // Call the saveBlog method from the BlogService
+    this.saveService.saveBlog(this.blogId)
+      .then(response => {
+        // Handle success
+        console.log('Blog saved successfully', response);
+      })
+      .catch(error => {
+        // Handle error
+        console.error('Failed to save blog', error);
+      });
   }
 }
 
---
+++ 06_Frontend/src/app/services/save-collection.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+// import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Injectable({
+  providedIn: "root",
+})
+export class SaveCollectionService {
+  private url = "http://localhost:8080/users";
+  private token: string | null = null;
+
+  constructor(
+    private http: HttpClient,
+    // private snackBar: MatSnackBar
+  ) {
+    this.token = this.getToken();
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error(error);
+    // this.snackBar.open('An error occurred. Please try again.', '', { duration: 3000 });
+    return Promise.reject(error);
+  }
+
+  private getHeaders(): { [header: string]: string } {
+    return this.token ? { 'Authorization': this.token } : {};
+  }
+
+  private getToken(): string | null {
+    return localStorage.getItem('Authorization');
+  }
+
+  saveBlog(blogId: string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.http.post<any>(`${this.url}/save_blog?blogId=${blogId}`, {}, { headers: this.getHeaders() })
+        .subscribe(
+          (res: any) => {
+            // this.snackBar.open('Blog saved successfully', '', { duration: 3000 });
+            console.log(res)
+            resolve(res);
+          },
+          error => {
+            this.handleError(error);
+            reject(error);
+          }
+        );
+    });
+  }
+
+  unsaveBlog(blogId: string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.http.post<any>(`${this.url}/unsave_blog?blogId=${blogId}`, {}, { headers: this.getHeaders() })
+        .subscribe(
+          (res: any) => {
+            // this.snackBar.open('Blog unsaved successfully', '', { duration: 3000 });
+            resolve(res);
+          },
+          error => {
+            this.handleError(error);
+            reject(error);
+          }
+        );
+    });
+  }
+
+  getSavedBlogs(): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.http.get<any>(`${this.url}/saved_blogs_by_user`, { headers: this.getHeaders() })
+        .subscribe(
+          (data: any) => {
+            console.log(data)
+            resolve(data);
+          },
+          error => {
+            this.handleError(error);
+            reject(error);
+          }
+        );
+    });
+  }
+}
