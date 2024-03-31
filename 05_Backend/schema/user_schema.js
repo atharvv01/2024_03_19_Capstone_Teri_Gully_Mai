5c48d5b9f7e120c60aa0c3714f674d2c40622689
Sumit4482 <sbanwakde4482@gmail.com>
2024-04-01 03:04:46 +0530

    Updated : User and Blog Model ,User API Endpoints

--- 05_Backend/schema/user_schema.js
+++ 05_Backend/schema/user_schema.js
@@ -1,32 +1,50 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 3,
-    maxlength: 30
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    match: /^\S+@\S+\.\S+$/
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  token:{
-    type:String,
-    default:''
-  }
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        maxlength: 30,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: /^\S+@\S+\.\S+$/
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    profilePicture: String,
+    points: {
+        type: Number,
+        default: 0
+    },
+    premiumStatus: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    token: {
+        type: String,
+        default: ''
+    }
 });
 
+const User = mongoose.model('User', userSchema);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = User;
